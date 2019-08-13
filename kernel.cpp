b0cffd0237825c82ba750140cfcf7be777cb92b5
root <root@etc-laptop.localdomain>
2019-08-13 11:13:12 -0700

    Add types

--- kernel.cpp
+++ kernel.cpp
@@ -1,12 +1,24 @@
+#include "types.h"
+
 void printf(char *str)
 {
-    unsigned short *VideoMemory = (unsigned short *)0xb8000;
+    uint16_t *VideoMemory = (uint16_t *)0xb8000;
 
     for (int i = 0; str[i] != '\0'; i++)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
-extern "C" void kernelMain(void *multiboot_structure, unsigned int magicnumber)
+// typedef void (*constructor)();
+// extern "C" constructor start_ctors;
+// extern "C" constructor end_ctors;
+// extern "C" void callConstuctors()
+// {
+//     // jmp into each func pointer between start and end ctors
+//     for (constructor *i = &start_ctors; *i != end_ctors; i++)
+//         (*i)();
+// }
+
+extern "C" void kernelMain(void *multiboot_structure, uint32_t magicnumber)
 {
     printf("Hello World");
 
